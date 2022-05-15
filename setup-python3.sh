0364734ba0a36a385c7534cccbcf5ce2987fc3b8
Andreas Zoglauer <andreas@megalibtoolkit.com>
2022-05-15 09:16:15 -0700

    CHG: Fix for Intel Mac's

--- setup-python3.sh
+++ setup-python3.sh
@@ -53,7 +53,7 @@ if [[ "$?" != "0" ]]; then
 fi
 
 # Install tensorflow & torch the special way to take care of issues on Apple M1 machines
-if [[ $(uname -s) == *arwin* ]]; then 
+if [[ $(uname -s) == *arwin ]] && [[ $(uname -m) == arm64 ]]; then
   # HDF5 is troublesome, thus do this first
   P=$(which port); P=${P%/bin/port}
   if [[ -f ${P}/lib/libhdf5.dylib ]]; then
