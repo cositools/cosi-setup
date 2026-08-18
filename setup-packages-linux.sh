0cd107a77f7971af26bcbcf3b31d4b0048f0326a
Andreas Zoglauer <andreas@megalibtoolkit.com>
2026-08-18 15:10:44 -0700

    CHG: sudo squence

--- setup-packages-linux.sh
+++ setup-packages-linux.sh
@@ -134,8 +134,8 @@ fi
         echo "Performing automatic install of packages. I will do the following:"
         echo "sudo apt update; sudo apt install ${TOBEINSTALLED}"
         echo " "
-        DEBIAN_FRONTEND=noninteractive sudo apt update
-        DEBIAN_FRONTEND=noninteractive sudo apt install -y ${TOBEINSTALLED}
+        sudo DEBIAN_FRONTEND=noninteractive apt update
+        sudo DEBIAN_FRONTEND=noninteractive apt install -y ${TOBEINSTALLED}
         if [[ "$?" != "0" ]]; then
           echo " "
           echo "ERROR: Something went wrong with the autoamtic package installation."
